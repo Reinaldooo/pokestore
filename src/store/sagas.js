34e64a9a012253f5a81d02d765e6f4ffaf8b68ea
Reinaldo Trindade <rewifetri@gmail.com>
2020-07-01 15:30:00 -0300

    changed api address

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,21 +1,8 @@
-import { put, takeLatest, all, call } from 'redux-saga/effects'
-//
-import { createSlug, removerAcentos } from "../services/utils"
+import { put, takeLatest, all, call } from 'redux-saga/effects';
 
 function fetchProducts() {
-  return fetch("https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog")
+  return fetch("https://api.reinaldowft.com/fashion")
     .then((data) => data.json())
-    .then((data) => {
-      // filter to avoid empty results
-      data = data.filter((item) => item.name)
-      // Create some unique id for the products since a real API
-      // would have it
-      data.forEach((product) => {
-        product["id"] = createSlug(product.name) + "-" + product.code_color;
-        product["normalizedName"] = removerAcentos(product.name);
-      });
-      return data;
-    });
 }
 
 function* callFetchProducts() {
